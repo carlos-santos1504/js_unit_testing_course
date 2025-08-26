c21fd5d17da068bbe7b2a934e102ceafa35fb20b
carlos-santos1504 <carlos.santos1504@gmail.com>
2025-08-26 19:50:16 -0300

    ajuste de codigo

---
+++ 08-more-mocking/starting-project/util/validation.test.js
@@ -0,0 +1,15 @@
+import { it } from "vitest" 
+import { validateNotEmpty } from "./validation";
+
+it('should throw an error if an empty string is provided as a value', () => {
+    const testInput = '';
+    const validationFn = () => validateNotEmpty(testInput);
+    expect(validationFn).toThrow();
+});
+
+it('should throw an error if an empty string is provided as a value', () => {
+    const testInput = '  ';
+    const testErrorMessage = 'Test';
+    const validationFn = () => validateNotEmpty(testInput,testErrorMessage);
+    expect(validationFn).toThrow(testErrorMessage);
+});
